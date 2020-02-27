b3ec1fcf09a72bab1730d389cc48d300c8a150ca
Rakesh Senthilvelan <rsenthil@ucsd.edu>
2020-02-27 11:04:39 -0800

    Fix to MapQuest

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,8 +1,8 @@
 function initMap() {
-	L.mapquest.key = 'yWRKiBGBL2joXn1vVlIa9KHcVHSyLbPy';
+	L.mapQuest.key = 'yWRKiBGBL2joXn1vVlIa9KHcVHSyLbPy';
 
-  var map = L.mapquest.map('map', {center" [32.88, -117.236],
-  layers: L.mapquest.tileLayer('map'), zoom:12, zoomControl: false});
+  var map = L.mapQuest.map('map', {center" [32.88, -117.236],
+  layers: L.mapQuest.tileLayer('map'), zoom:12, zoomControl: false});
 
   L.marker([32.88,-117.236]).addTo(map);
 }
